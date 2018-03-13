ff3d7d87696511b555401dcb7390ddc7c3c45409
Robert McDermott <robert.c.mcdermott@gmail.com>
2018-03-13 15:12:55 -0700

    initial

---
+++ fizzbuzz.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Fprintf(os.Stderr, "\nA number is required\n")
+		os.Exit(1)
+	}
+	num, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "\nPlease provide an integer only!\n")
+		os.Exit(1)
+	}
+	fmt.Printf("FizzBuzz for %d:\n", num)
+	for _, n := range fizzBuzz(num) {
+		fmt.Println(n)
+	}
+}
+
+func fizzBuzz(num int) []string {
+	res := []string{}
+	for i := 1; i <= num; i++ {
+		t := ""
+		//fmt.Println(i)
+		if i%3 == 0 {
+			t += "Fizz"
+		}
+		if i%5 == 0 {
+			t += "Buzz"
+		}
+		if len(t) == 0 {
+			t = strconv.Itoa(i)
+		}
+		res = append(res, t)
+	}
+	return res
+}
